f2ed92f693329687949ab40139217e30ca3b0b4a
stcmcd <steve.mcdonaldd@gmail.com>
2020-10-23 17:49:53 +0100

    first

--- src/test/java/steve/step_definitions/AreaTriangle.java
+++ src/test/java/steve/step_definitions/AreaTriangle.java
@@ -7,14 +7,6 @@ class AreaTriangle {
 
     @Test
     public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Width of triangle:");
-        double width = scanner.nextDouble();
-
-        System.out.println("Height of triangle:");
-        double height = scanner.nextDouble();
-
         double area = (width* height)/2;
         System.out.println("Area of Triangle: " + area);
     }
--- src/test/java/steve/step_definitions/hclStepDefs.java
+++ src/test/java/steve/step_definitions/hclStepDefs.java
@@ -1,27 +1,39 @@
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Then;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseOptions;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import steve.BikesExtension;
-import steve.step_definitions.RestAss.pojo.Bikes;
-import steve.step_definitions.RestAss.pojo.Locations;
-import java.util.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 /// this is  aa comment again, again
 
-
 public class hclStepDefs {
-
+    private static final String CHROME_DRIVER_PATH = "C:\\WebDrivers\\Chrome85\\chromedriver.exe";
 
     @Then("^print the area of a triangle with (\\d+) and height (\\d+)$")
     public static void printAreaOfTriangle(int width, int height ) throws Throwable {
+        double area = (width* height)/2;
+        System.out.println("Area of Triangle: " + area);
     }
 
     @Then("^does the Founder and Chairman exist$")
-    public static void founderAndChairman(String basePath, DataTable dataTable) throws Throwable {
+    public static void founderAndChairman() throws Throwable {
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+
+        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.hcl.com/");
+
+        WebElement el;
+
+//        el = driver.findElement(By.xpath(".//a[@href = 'https://hcl.com/leadership/']"));
+//        el = driver.findElement(By.xpath(".//a[contains(text(), 'Leadership')]"));
+        driver.get("https://hcl.com/leadership/");
+
+        assertTrue(driver.findElement(By.xpath(".//b[contains(text(), 'Founder & Chairman')]")).isDisplayed());
     }
 
     @Then("^Screenshote$")
